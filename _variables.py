66fedca38469ef6906e99da08c95298f59cceeb8
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-04-16 22:17:53 +0600

    implement team selection and team log in

--- _variables.py
+++ _variables.py
@@ -9,4 +9,8 @@ DATE = date.today().strftime(DATE_FORMAT)
 NOW = datetime.now().strftime("%I:%M:%S %p")
 TIME_STAMP = f"{DATE} - {NOW}"
 
+# Web element css selectors
 MS_ACC_LOG_IN = "div.table[role='button']"
+DROPDOWN_SELECT_TEAM = "button.guest-license-error-dropdown.ts-sym[type='button']"
+CHOOSE_TEAM = "ul.guest-license-error-menu li:nth-child(2) span.tenant-name"
+TEAM_LOG_IN = "button.guest-license-error-button"
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from _controller import Quinn
 from _driver import DRIVER
-from _variables import (URL, MS_ACC_LOG_IN)
+from _variables import (URL, MS_ACC_LOG_IN, DROPDOWN_SELECT_TEAM, CHOOSE_TEAM, TEAM_LOG_IN)
 
 DRIVER.get(URL)
 DRIVER.maximize_window()
@@ -8,5 +8,8 @@ quinn = Quinn(DRIVER)
 
 quinn.add_date()
 quinn.click_on('MS ACC log in', MS_ACC_LOG_IN)
+quinn.click_on("Dropdown select team", DROPDOWN_SELECT_TEAM)
+quinn.click_on("team selector", CHOOSE_TEAM)
+quinn.click_on("team log in", TEAM_LOG_IN)
 
 DRIVER.quit()
