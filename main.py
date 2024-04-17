630403f4f632aa0e4b0898e1b6e39d19f8deb75c
ITS MAC <asifchowdhuryrafi143@outlook.com>
2024-04-17 23:40:45 +0600

    Add click on chat icon

--- main.py
+++ main.py
@@ -1,6 +1,13 @@
 from _controller import Quinn
 from _driver import DRIVER
-from _variables import (URL, MS_ACC_LOG_IN, DROPDOWN_SELECT_TEAM, CHOOSE_TEAM, TEAM_LOG_IN)
+from _variables import (
+    URL,
+    MS_ACC_LOG_IN,
+    DROPDOWN_SELECT_TEAM,
+    CHOOSE_TEAM,
+    TEAM_LOG_IN,
+    CHAT_ICON,
+)
 
 DRIVER.get(URL)
 DRIVER.maximize_window()
@@ -11,5 +18,6 @@ quinn.click_on('MS ACC log in', MS_ACC_LOG_IN)
 quinn.click_on("Dropdown select team", DROPDOWN_SELECT_TEAM)
 quinn.click_on("team selector", CHOOSE_TEAM)
 quinn.click_on("team log in", TEAM_LOG_IN)
+quinn.click_on("Chat icon", CHAT_ICON)
 
 DRIVER.quit()
